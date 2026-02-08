f9c7f02caf08ed1911b3d3165467af79796a90da
joeloo1 <agboolajoel123@gmail.com>
2026-02-08 19:02:24 +0100

    feat: add logger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import app from "./app";
 import config from "./config/config.env";
 import { connectDB } from "./config/database";
+import logger from "./config/winston";
 
 const port = config.PORT;
 
 connectDB();
 app.listen(port, () => {
+  logger.info(`Server running on PORT: ${port}...`);
   console.log(`Server running on PORT: ${port}... `);
 });
