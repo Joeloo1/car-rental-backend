01b2376d33215cd7bf69ec566eccf120cc3c3a84
joeloo1 <agboolajoel123@gmail.com>
2026-02-10 11:00:23 +0100

    feat(auth): add auth controller for signup and email verification

---
+++ src/controllers/auth/auth.controller.ts
@@ -0,0 +1,56 @@
+import { NextFunction, Request, Response } from "express";
+import {
+  singupService,
+  verifyEmailService,
+  resendverifyEmailService,
+} from "../../services/auth/auth.service";
+import catchAsync from "../../utils/catchAsync";
+import logger from "../../config/winston";
+import AppError from "../../utils/AppError";
+
+// signup
+export const signup = catchAsync(async (req: Request, res: Response) => {
+  const { newUser, refreshToken, accessToken } = await singupService(req.body);
+
+  logger.info(`User with email: ${newUser.email} Signed Up successfully`);
+  res.status(201).json({
+    statu: "success",
+    data: { newUser },
+    token: { refreshToken, accessToken },
+  });
+});
+
+// email verification
+export const verifyEmail = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.query.token as string;
+
+    if (!token) {
+      return next(new AppError("verification token missing", 400));
+    }
+
+    await verifyEmailService(token);
+
+    res.status(200).json({
+      message: "Email Verified Successfully",
+    });
+  },
+);
+
+// Re-send verification name
+export const resendverifyEmail = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { email } = req.body;
+
+    if (!email) {
+      logger.warn("Email is required");
+      return next(new AppError("Email is required", 400));
+    }
+
+    await resendverifyEmailService(email);
+
+    res.status(200).json({
+      message: "Verification email sent successfully",
+    });
+  },
+);
