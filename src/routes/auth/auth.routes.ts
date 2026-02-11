c26f80b83835637961d0ed8d9f27e7bcaa8b09c6
joeloo1 <agboolajoel123@gmail.com>
2026-02-11 15:50:57 +0100

    feat(auth): add login, forgot password, reset password  routes

--- src/routes/auth/auth.routes.ts
+++ src/routes/auth/auth.routes.ts
@@ -2,15 +2,23 @@ import { Router } from "express";
 import {
   resendverifyEmail,
   signup,
+  login,
   verifyEmail,
+  forgotPassword,
+  resetPassword,
 } from "../../controllers/auth/auth.controller";
 import { validateRequest } from "../../middleware/validation_middleware";
-import { SignupSchema } from "../../schema/auth.schema";
+import { SignupSchema, LoginSchema } from "../../schema/auth.schema";
+import { adminSignup } from "../../controllers/admin/admin.controller";
 
 const router = Router();
 
+router.route("/admin/signup").post(validateRequest(SignupSchema), adminSignup);
 router.route("/signup").post(validateRequest(SignupSchema), signup);
+router.route("/login").post(validateRequest(LoginSchema), login);
 router.route("/verify-email").get(verifyEmail);
 router.route("/resend-verification-email").post(resendverifyEmail);
+router.route("/forgot-Password").post(forgotPassword);
+router.route("/reset-password/:token").patch(resetPassword);
 
 export default router;
