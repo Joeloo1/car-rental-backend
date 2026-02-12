ea712ba32e735f1e843bdf69d9aa2ba8005176e7
joeloo1 <agboolajoel123@gmail.com>
2026-02-12 20:23:42 +0100

    chore(config): add JWT, email, and client environment variables to config

--- src/config/config.env.ts
+++ src/config/config.env.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import { StringValue } from "ms";
 
 dotenv.config();
 
@@ -6,6 +7,16 @@ const config = {
   NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT,
   DIRECT_URL: process.env.DIRECT_URL,
+  JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET!,
+  JWT_REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET!,
+  ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY as StringValue,
+  REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY as StringValue,
+  EMAIL_USER: process.env.EMAIL_USER,
+  EMAIL_PASS: process.env.EMAIL_PASS,
+  EMAIL_HOST: process.env.EMAIL_HOST,
+  EMAIL_PORT: process.env.EMAIL_PORT,
+  VERIFICATION_SECRET: process.env.VERIFICATION_SECRET!,
+  CLIENT_URL: process.env.CLIENT_URL,
 };
 
 export default config;
