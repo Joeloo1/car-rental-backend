a97757c9c08ec30f7bdd6b8c049ef28fdfca0283
Agboola Joel <167136935+Joeloo1@users.noreply.github.com>
2026-02-14 15:14:19 +0100

    Remove unused import from validation middleware

--- src/middleware/validation_middleware.ts
+++ src/middleware/validation_middleware.ts
@@ -1,4 +1,4 @@
-import e, { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { ZodError, ZodTypeAny, ZodIssue } from "zod";
 import AppError from "../utils/AppError";
 
