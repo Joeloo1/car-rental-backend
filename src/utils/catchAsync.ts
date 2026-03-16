5b7bc1dc56b0787ed60db2ca2bc0d3a7c4d049c9
Joeloo1 <agboolajoel123@gmail.com>
2026-03-16 17:56:05 +0100

    make request type generic in catchAsync

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,14 +1,27 @@
 import { Request, Response, NextFunction } from "express";
 
-type AsyncFunction = (
-  req: Request,
+// type AsyncFunction = (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction,
+// ) => Promise<void>;
+//
+// const catchAsync = (fn: AsyncFunction) => {
+//   return (req: Request, res: Response, next: NextFunction) => {
+//     fn(req, res, next).catch(next);
+//   };
+// };
+//
+
+export type AsyncFunction<T extends Request = Request> = (
+  req: T,
   res: Response,
   next: NextFunction,
 ) => Promise<void>;
 
-const catchAsync = (fn: AsyncFunction) => {
+const catchAsync = <T extends Request = Request>(fn: AsyncFunction<T>) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    fn(req, res, next).catch(next);
+    fn(req as T, res, next).catch(next);
   };
 };
 
